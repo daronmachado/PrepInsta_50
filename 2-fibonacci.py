92118193f75b46bb2bfaddf54a3a0132dbb5b589
daronmachado <daronmachado22@gmail.com>
2025-01-28 16:05:03 +0530

    fibonacci upo nth term

--- 2-fibonacci.py
+++ 2-fibonacci.py
@@ -1,10 +1,12 @@
 def fibonacci(n):
     a=0
     b=1
+    print(a,b,end=" ")
     for _ in range(n-2):
         res = a + b
         a = b
         b = res
-    print(res)
+        print(res,end=" ")
+    
 
 fibonacci(7)
